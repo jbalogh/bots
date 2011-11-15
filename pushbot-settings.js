2c73d66fa2dc8b727dadda50004e5787e1b8a987
James Socol <james@mozilla.com>
2011-11-15 13:47:33 -0500

    Make redis host/port configurable.

--- pushbot-settings.js
+++ pushbot-settings.js
@@ -4,6 +4,8 @@ var options = [
         channel: '#remora',
         /* Bot name. */
         name: 'pushbot',
+        /* Redis host:port */
+        redis: 'localhost:6379',
         /* Redis pubsub channel name. */
         pubsub: 'deploy.addons',
         /* URL to the chief log directory. */
@@ -15,11 +17,12 @@ var options = [
         /* URL to the github repo. */
         github: 'https://github.com/mozilla/zamboni/',
         /* Name of the site. */
-        site: 'zamboni'
+        site: 'zamboni',
     },
     {
         channel: '#remora',
         name: 'stagebot',
+        redis: 'localhost:6379',
         pubsub: 'deploy.addons-stage',
         logs: 'http://addonsadm.private.phx1.mozilla.com/chief/addons.stage/logs/',
         revision: 'https://addons-stage.allizom.org/media/git-rev.txt',
